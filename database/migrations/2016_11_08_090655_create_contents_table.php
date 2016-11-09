9e956128b35d6a1ae5a209e7df192a6c7f4370e2
serious <fikurimufc@gmail.com>
2016-11-09 09:47:30 +0700

    First commit, untuk project crud image

---
+++ database/migrations/2016_11_08_090655_create_contents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('img_name');
+            $table->string('thumb_size');
+            $table->string('img');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
